07e0574a75c723ea14e80b5313a7a437b04e3ed5
u31504 <u31504@P31504>
2022-12-03 09:40:00 +1000

    Всё прекрасно

--- Program.cs
+++ Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Testirovanie_MathTask_Samkova_PR2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        public class Geometry
+        {
+            public int RectangleArea(int a, int b)
+            {
+                return a * b;
+            }
         }
     }
 }
